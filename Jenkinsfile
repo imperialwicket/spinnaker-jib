18769254548c19e7cb8e342143805b0a76324de5
Nicholas Whittier <imperialwicket@gmail.com>
2017-12-13 15:28:47 -0700

    Add versioning capabilities in build and Jenkinsfile.

--- Jenkinsfile
+++ Jenkinsfile
@@ -15,7 +15,7 @@ pipeline {
         }
         stage('Test') {
             steps {
-                echo 'Testing something else...'
+                sh './build.sh $(git rev-parse HEAD)'
             }
         }
     }
--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@
 
 release=$1
 buildDir=build
+rootDir=$(pwd)
 echo "Building spinnaker-jib project..."
 sleep 5
 
@@ -19,9 +20,13 @@ mkdir $buildDir
 cp src/* $buildDir
 sed -i s/VERSION/${release:-Unknown}/ $buildDir/index.html
 
+cd $buildDir
+tar czf $release.tar.gz *
+cd $rootDir
+
 echo "Pushing this release to s3..."
 sleep 2
-echo "TODO: add an 'aws s3 cp build/* s3://someBucket/$release/*'"
+aws s3 cp $buildDir/$release.tar.gz s3://someBucket/spinnaker-jib/
 
 # There is no try, only do.
 exit 0
