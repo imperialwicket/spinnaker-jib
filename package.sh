5231070e24c509d517da974a67d10156bb6a7b18
Nicholas Whittier <nicholas@hellosign.com>
2017-12-15 09:30:53 -0700

    Convert build to deb package

---
+++ package.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# packaging script for spinnaker-jib demo, puts a flat file
+# release into a deb package for "artifacting" and packing via
+# packer/spinnaker
+
+# Expects an arg representative of the package version (SEMVER-COMMIT)
+
+# Expect to run after 'build.sh' script which manages the 'build' directory
+version=$1
+buildfolder=spinnaker-jib_$version
+debdir=build/$buildfolder
+mkdir -p $debdir
+mkdir -p $debdir/opt/spinnaker-jib
+mkdir -p $debdir/etc/nginx/sites-available/
+cp -r package/DEBIAN $debdir
+cp package/nginx-spinnaker-jib $debdir/etc/nginx/sites-available/spinnaker-jib
+# Breaks for non-html files and subdirs...
+mv build/*.html $debdir/opt/spinnaker-jib/
+
+# Version:
+sed -i "s/VERSION/$1/" $debdir/DEBIAN/control
+echo "Building version $1"
+
+# build it in 'build' so cleanup is easier (delete the build dir)
+cd build
+dpkg-deb --build $buildfolder
